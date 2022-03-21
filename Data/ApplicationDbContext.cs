2206de11b3d415f26c6c60d48ff6b1c45999d3b1
Anton <antonhogstrom1994@gmail.com>
2022-03-21 19:46:15 -0300

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyPortfolio;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Course>? Course { get; set; }
+    public DbSet<Project>? Project { get; set; }
+    public DbSet<Category>? Category { get; set; }
+    public DbSet<SocialMedia>? SocialMedia { get; set; }
+}
